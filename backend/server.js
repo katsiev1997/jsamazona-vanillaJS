38f6391b9d752bb1f98e174736a396b42a6a0f94
katsiev1997 <katsiev1997@gmail.com>
2024-02-01 16:04:20 +0300

    eslint

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
-import express from "express";
-import cors from "cors";
-import data from"./data";
+import express from 'express';
+import cors from 'cors';
+import data from './data';
 
 const app = express();
 
 app.use(cors());
-app.get("/api/products", (req, res) => {
+app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
 
 app.listen(5000, () => {
-  console.log("Server running on port 5000");
+  console.log('Server running on port 5000');
 });
